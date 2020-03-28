c63a9d0bc2bcc9f52da0a503a0085a3c5883ee56
Wendel Freitas <wendelfreitasbranco@gmail.com>
2020-03-28 17:05:57 -0300

    feat(404): add style to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
+export const Wrapper = styled.div`
+  display: flex;
+  position: absolute;
+  flex: 1;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 150px;
+    color: #dd7e09;
+    padding: 0;
+    margin: 20px;
+  }
+
+  p {
+    color: white;
+    font-weight: 500;
+    font-style: italic;
+    font-size: 25px;
+  }
+`;
+
 const NotFoundPage = () => (
-  <Layout>
+  <Layout padding={2}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Wrapper>
+      <h1>404</h1>
+      <p>Já que voce chegou até aqui, vou te contar um segredo:</p>
+      <br />
+      <p>Eu ainda não pisei na lua...</p>
+    </Wrapper>
   </Layout>
 );
 
