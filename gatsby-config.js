66c2a2b8eed10b0ee1f3c616d23774b61fe25601
Wendel Freitas <wendelfreitasbranco@gmail.com>
2020-03-29 20:25:15 -0300

    chore: add gatsby remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,20 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: []
+      }
+    },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
