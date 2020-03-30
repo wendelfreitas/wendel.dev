e1d427ca43a7d1da060e7e6580ee4f953d13159d
Wendel Freitas <wendelfreitasbranco@gmail.com>
2020-03-30 20:31:45 -0300

    feat: add prismjs and create pagination

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,11 +59,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Wendel de Freitas Branco`,
+        short_name: `Wendel Freitas`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#222`,
+        theme_color: `#dd7e09`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
--- gatsby-node.js
+++ gatsby-node.js
@@ -75,5 +75,21 @@ exports.createPages = ({ graphql, actions }) => {
         }
       });
     });
+
+    const postsPerPage = 7;
+    const numPages = Math.ceil(posts.length / postsPerPage);
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? `/blog` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1
+        }
+      });
+    });
   });
 };
