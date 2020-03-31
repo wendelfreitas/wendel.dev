40bb57f425e18144108ba5cbb52023a19dfde9cc
Wendel Freitas <wendelfreitasbranco@gmail.com>
2020-03-31 16:52:21 -0300

    feat: add disqus to the app

---
+++ src/components/Comments/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDisqusComments from 'react-disqus-comments';
+
+import { Wrapper, Title } from './styles';
+
+const Comments = ({ url, title }) => {
+  const completeURL = `https://willianjusten.com.br${url}`;
+
+  return (
+    <Wrapper>
+      <Title>Comentários</Title>
+      <ReactDisqusComments
+        shortname="wendelfb"
+        identifier={completeURL}
+        title={title}
+        url={completeURL}
+      />
+    </Wrapper>
+  );
+};
+
+Comments.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default Comments;
---
+++ src/components/Comments/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+export const Wrapper = styled.section`
+  margin: auto;
+  width: 500px;
+  max-width: 100rem;
+  padding: 3rem 10rem 3rem;
+  ${media.lessThan('large')`
+    padding: 3rem 1.4rem 0;
+    max-width: 100%;
+  `}
+  iframe[src*='ads-iframe'] {
+    display: none;
+  }
+  #disqus_thread {
+    a {
+      color: ${({ theme }) => theme.orange} !important;
+    }
+  }
+`;
+
+export const Title = styled.h2`
+  color: ${({ theme }) => theme.orange};
+  font-size: 2.1rem;
+  font-weight: 700;
+  padding-bottom: 2rem;
+  ${media.lessThan('large')`
+    font-size: 1.375rem;
+  `}
+`;
