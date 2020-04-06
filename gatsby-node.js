ee51357282ff2ff014855a7b4eb2d36955bd35eb
Wendel Freitas <wendelfreitasbranco@gmail.com>
2020-04-06 20:14:11 -0300

    chore: add image to seo inside post

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,6 +36,7 @@ exports.createPages = ({ graphql, actions }) => {
               date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
               description
               title
+              image
             }
             timeToRead
           }
